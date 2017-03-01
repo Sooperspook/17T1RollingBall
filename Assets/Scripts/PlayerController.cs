812c9ecdd48f75a77f7fa3d12c013466ccbabb5d
Sooperspook <nlinggod@gmail.com>
2017-03-01 23:15:18 +1000

    Basics of game
    Simple player model, basic ground and basic pick ups.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float torqueSpeed;
+    private Rigidbody player;
+
+    void Start()
+    {
+        player = GetComponent<Rigidbody>();
+    }
+
+void FixedUpdate ()
+    {
+
+        // Using torque to move the player.
+        // moveForeward also moves backward, moveTurn causes it to spin
+        // problem: once it starts moving, because axes move, hard to control
+
+        float moveForeward = Input.GetAxis("Vertical");
+        player.AddTorque(transform.forward * torqueSpeed * moveForeward);
+
+        float moveTurn = Input.GetAxis("Horizontal");
+        player.AddTorque(transform.up * torqueSpeed * moveTurn);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+       if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+        }
+
+   
+    }
+    
+}
+
