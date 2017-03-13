c7b7f248762b282c024dccb4a0ba3c2d5a073cab
Sooperspook <nlinggod@gmail.com>
2017-03-13 15:26:59 +1000

    Basic game done
    Very basic mechanics and menu screen done

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,15 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 
     public float torqueSpeed;
+    public Text countText;
+    public Text winText;
+
     private Rigidbody player;
+    private int count;
 
     void Start()
     {
         player = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText ();
+        winText.text = "";
     }
 
 void FixedUpdate ()
@@ -26,15 +34,26 @@ void FixedUpdate ()
         player.AddTorque(transform.up * torqueSpeed * moveTurn);
     }
 
+    // Upon touching another object, test what kind of object and act accordingly
     void OnTriggerEnter(Collider other)
     {
        if (other.gameObject.CompareTag("Pick Up"))
         {
             other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText ();
         }
 
    
     }
-    
+
+    void SetCountText ()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 6)
+        {
+            winText.text = "You Win";
+        }
+    }
 }
 
